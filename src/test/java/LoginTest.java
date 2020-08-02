2722998f83b9df9ba4c9fbaf562d0bd75731e979
Ananda Sen <ananda.sen@q2ebanking.com>
2020-08-02 22:44:42 +0530

    first commit

---
+++ src/test/java/LoginTest.java
@@ -0,0 +1,29 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+	public static WebDriver driver = null;
+
+	@BeforeSuite
+	public void setup() {
+		driver = new ChromeDriver();
+		System.out.println("In Setup");
+
+	}
+
+	@AfterSuite
+	public void tearDown() {
+		System.out.println("In Teardown");
+	}
+
+	@Test
+	public void testCase() {
+		System.out.println("In Test Case");
+		
+
+	}
+
+}
